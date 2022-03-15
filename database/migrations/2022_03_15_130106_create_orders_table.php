7c27ff6608bc72a2c6b75c93a1cd244a23f8afdc
doviethung2712 <anhboy6ss@gmail.com>
2022-03-15 20:21:45 +0700

    update db

---
+++ database/migrations/2022_03_15_130106_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seat_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->unsignedBigInteger('food_id');
+            $table->dateTime('date');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('seat_id')->references('id')->on('seats');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->foreign('food_id')->references('id')->on('foods');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}

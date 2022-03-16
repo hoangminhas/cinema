43ee7d48b45457f234a3edd6905a4ac85e34ea2c
hoangminhas <hoangminh9973@gmail.com>
2022-03-16 20:16:26 +0700

    last update login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,8 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <form action="">
-            <button type="button">Click</button>
-        </form>
-        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -130,6 +127,6 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
     </body>
 </html>

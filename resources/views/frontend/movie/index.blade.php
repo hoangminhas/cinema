711ce6c00c5f854fdf6057f923e7f1d770214a4e
hoangminhas <hoangminh9973@gmail.com>
2022-03-20 03:45:55 +0700

    lastest update

--- resources/views/frontend/movie/index.blade.php
+++ resources/views/frontend/movie/index.blade.php
@@ -12,12 +12,12 @@
                         <a href="{{route('current.movie.show',$movie->id)}}"><img src="{{$movie->image}}" class="card-img-top" alt="movie"></a>
                         <div class="card-body">
                             <h5 class="card-title">{{$movie->name}}</h5>
-                            <p class="card-text">{{$movie->summary}}</p>
-{{--                            <p class="card-text">--}}
-{{--                                Thể loại: @foreach($movie->categories as $category)--}}
-{{--                                              <span>{{$category->name}}</span>--}}
-{{--                                @endforeach--}}
-{{--                            </p>--}}
+                            <p class="card-text">{{ $movie->duration }} phút</p>
+                            <p class="card-text">
+                                Thể loại: @foreach($movie->categories as $category)
+                                              <span>{{$category->name}}</span>
+                                @endforeach
+                            </p>
                             <a href="{{route('showFormOrder', $movie->id)}}" class="btn btn-danger">Đặt Vé</a>
                         </div>
                     </div>
@@ -34,11 +34,11 @@
             @foreach ($movies as $movie)
                 @if ($movie->date > now())
                     <div class="col-4" style="margin: 15px 0px">
-                        <div class="card" style="width: 25rem; height: 35em">
+                        <div class="card" style="width: 25rem; height: 33em">
                             <img src="{{ $movie->image }}" class="card-img-top" alt="movie">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $movie->name }}</h5>
-                                <p class="card-text">{{ $movie->summary }}</p>
+                                <p class="card-text">{{ $movie->duration }} phút</p>
                             </div>
                         </div>
                     </div><br><br>
